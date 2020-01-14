b88d1fc0d8947418f0939a9983ecdf7667344a4d
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2020-01-14 19:15:18 +0200

    WIP for generalized Protos deployment on a cloud. Deploying Protos instances works via the Scaleway provider but it's not feature complete.

--- cmd/protos/protos.go
+++ cmd/protos/protos.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"os/signal"
+	"os/user"
+	"syscall"
 
 	survey "github.com/AlecAivazis/survey/v2"
+	"github.com/pkg/errors"
 	"github.com/protosio/cli/internal/cloud"
-	log "github.com/sirupsen/logrus"
+	ssh "github.com/protosio/cli/internal/ssh"
+	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 )
 
@@ -110,7 +116,24 @@ func transformCredentials(creds map[string]interface{}) map[string]string {
 	return transformed
 }
 
+func catchSignals(sigs chan os.Signal, quit chan interface{}) {
+	<-sigs
+	quit <- true
+}
+
 func protosInit() error {
+
+	log := logrus.New()
+	log.SetLevel(logrus.DebugLevel)
+
+	// create config and state directory
+	usr, _ := user.Current()
+	protosDir := usr.HomeDir + "/.protos"
+	log.Infof("Creating Protos directory '%s'", protosDir)
+	err := os.MkdirAll(protosDir, os.FileMode(0600))
+	if err != nil {
+		return errors.Wrap(err, "Failed to initialize Protos")
+	}
 
 	// select cloud provider
 	var cloudProvider string
@@ -138,21 +161,79 @@ func protosInit() error {
 		return err
 	}
 
-	// upload protos image
-	err = client.AddImage("https://releases.protos.io/test/scaleway-efi.iso", "4b49901e65420b55170d95768f431595")
+	imageID := ""
+	images, err := client.GetImages()
 	if err != nil {
-		return err
+		return errors.Wrap(err, "Failed to initialize Protos")
 	}
+	if id, found := images["protos-image"]; found == true {
+		log.Infof("Found latest Protos image (%s) in your infra cloud account", id)
+		imageID = id
+	} else {
+		// upload protos image
+		log.Info("Latest Protos image not in your infra cloud account. Adding it.")
+		imageID, err = client.AddImage("https://releases.protos.io/test/scaleway-efi.iso", "4b49901e65420b55170d95768f431595")
+		if err != nil {
+			return errors.Wrap(err, "Failed to initialize Protos")
+		}
+	}
+
+	// create SSH key used for instance
+	log.Info("Generating SSH key for the new VM instance")
+	key, err := ssh.GenerateKey()
+	if err != nil {
+		return errors.Wrap(err, "Failed to initialize Protos")
+	}
+
+	// create protos data volume
 
 	// deploy a protos instance
-
-	// get user details
-	userDetails := userDetails{}
-	userDetailsQuestions := getUserDetailsQuestions(&userDetails)
-	err = survey.Ask(userDetailsQuestions, &userDetails)
+	vmName := "protos1"
+	log.Infof("Deploying Protos instance '%s' using image '%s'", vmName, imageID)
+	vmIP, err := client.NewInstance(vmName, imageID, key.Public())
 	if err != nil {
-		return err
+		return errors.Wrap(err, "Failed to initialize Protos")
 	}
+
+	// log.Info(key.EncodePrivateKeytoPEM())
+
+	// test SSH and create SSH tunnel used for initialisation
+	tempClient, err := ssh.NewConnection(vmIP, "root", key.SSHAuth(), 10)
+	if err != nil {
+		return errors.Wrap(err, "Failed to initialize Protos")
+	}
+	tempClient.Close()
+	log.Info("Instance is ready")
+
+	log.Infof("Creating SSH tunnel to the new VM, using ip '%s'", vmIP)
+	tunnel := ssh.NewTunnel(vmIP+":22", "root", key.SSHAuth(), "localhost:8080", log)
+	localPort, err := tunnel.Start()
+	if err != nil {
+		return errors.Wrap(err, "Failed to initialize Protos")
+	}
+	log.Infof("Please do the setup using a browser at 'http://localhost:%d/'. Once finished, press CTRL+C to continue", localPort)
+
+	quit := make(chan interface{}, 1)
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go catchSignals(sigs, quit)
+	<-quit
+
+	log.Info("CTRL+C received. Shutting down SSH tunnel")
+	err = tunnel.Close()
+	if err != nil {
+		return errors.Wrap(err, "Failed to initialize Protos")
+	}
+	log.Info("SSH tunnel terminated successfully")
+	log.Infof("Protos instance '%s' - '%s' deployed successfully", vmName, vmIP)
+
+	// // get user details
+	// userDetails := userDetails{}
+	// userDetailsQuestions := getUserDetailsQuestions(&userDetails)
+	// err = survey.Ask(userDetailsQuestions, &userDetails)
+	// if err != nil {
+	// 	return err
+	// }
 
 	return nil
 }
