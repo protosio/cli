6b6cf8128add4cd7e3a7ff262156d4b2adba70af
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2020-01-14 19:10:03 +0200

    added a new module with various SSH utilities

--- internal/ssh/ssh.go
+++ internal/ssh/ssh.go
@@ -2,9 +2,8 @@ package ssh
 
 import (
 	"crypto/rand"
-	"encoding/pem"
+	"time"
 
-	"github.com/mikesmitty/edkey"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/crypto/ed25519"
@@ -12,23 +11,25 @@ import (
 )
 
 // GenerateKey generates a SSH key pair
-func GenerateKey() ([]byte, string, error) {
-	pubKey, privKey, err := ed25519.GenerateKey(rand.Reader)
+func GenerateKey() (Key, error) {
+	key := Key{}
+	var err error
+	key.public, key.private, err = ed25519.GenerateKey(rand.Reader)
 	if err != nil {
-		return nil, "", errors.Wrap(err, "Failed to generate SSH key")
+		return key, errors.Wrap(err, "Failed to generate SSH key")
 	}
-	publicKey, err := ssh.NewPublicKey(pubKey)
-	if err != nil {
-		return nil, "", errors.Wrap(err, "Failed to generate SSH key")
-	}
+	// publicKey, err := ssh.NewPublicKey(key.public)
+	// if err != nil {
+	// 	return key, errors.Wrap(err, "Failed to generate SSH key")
+	// }
 
-	pemKey := &pem.Block{
-		Type:  "OPENSSH PRIVATE KEY",
-		Bytes: edkey.MarshalED25519PrivateKey(privKey),
-	}
-	privateKey := pem.EncodeToMemory(pemKey)
-	authorizedKey := ssh.MarshalAuthorizedKey(publicKey)
-	return privateKey, string(authorizedKey), nil
+	// pemKey := &pem.Block{
+	// 	Type:  "OPENSSH PRIVATE KEY",
+	// 	Bytes: edkey.MarshalED25519PrivateKey(privKey),
+	// }
+	// privateKey := pem.EncodeToMemory(pemKey)
+	// authorizedKey := ssh.MarshalAuthorizedKey(publicKey)
+	return key, nil
 }
 
 // ExecuteCommand opens a session using the provided client and executes the provided command
@@ -59,4 +60,31 @@ func ExecuteCommand(cmd string, client *ssh.Client) (string, error) {
 
 	return string(output), nil
 
+}
+
+func NewConnection(host string, user string, auth ssh.AuthMethod, maxRetries int) (*ssh.Client, error) {
+	sshConfig := &ssh.ClientConfig{
+		User: "root",
+		Auth: []ssh.AuthMethod{
+			auth,
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // TODO validate server before?
+	}
+
+	tries := 0
+	var client *ssh.Client
+	var err error
+	for {
+		tries++
+		if tries > maxRetries {
+			return nil, errors.Wrapf(err, "Failed to open SSH connection to '%s@%s'", user, host)
+		}
+		client, err = ssh.Dial("tcp", host+":22", sshConfig) // TODO remove hardocoded port?
+		if err != nil {
+			time.Sleep(3 * time.Second)
+		} else {
+			break
+		}
+	}
+	return client, nil
 }
---
+++ internal/ssh/tunnel.go
@@ -0,0 +1,113 @@
+package ssh
+
+import (
+	"io"
+	"net"
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/ssh"
+)
+
+// Tunnel represents and SSH tunnel to a remote host
+type Tunnel struct {
+	sshHost   string
+	sshUser   string
+	sshAuth   ssh.AuthMethod
+	sshConn   *ssh.Client
+	listener  net.Listener
+	localPort int
+	target    string
+	log       *logrus.Logger
+	connMap   []chan interface{}
+}
+
+// Start initiates the ssh tunnel
+func (t *Tunnel) Start() (int, error) {
+	// setup the local listener using a random port
+	var err error
+	t.listener, err = net.Listen("tcp", "localhost:0")
+	if err != nil {
+		return 0, err
+	}
+	t.localPort = t.listener.Addr().(*net.TCPAddr).Port
+
+	// setup the SSH connection
+	sshConfig := &ssh.ClientConfig{
+		User: t.sshUser,
+		Auth: []ssh.AuthMethod{t.sshAuth},
+		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+			// Always accept key.
+			return nil
+		}}
+	t.sshConn, err = ssh.Dial("tcp", t.sshHost, sshConfig)
+	if err != nil {
+		return 0, err
+	}
+
+	// accept local connections and start the forwarding
+	go func() {
+		t.log.Info("Waiting for local connections to forward via the SSH tunnel")
+		for {
+			localConn, err := t.listener.Accept()
+			if err != nil {
+				if strings.Contains(err.Error(), "use of closed network connection") {
+					t.log.Debug("Local SSH tunnel listener closed. Not accepting any new connections.")
+					return
+				}
+				t.log.Errorf("Failed to accept connection via the SSH tunnel: %s", err)
+				continue
+			}
+			close := make(chan interface{})
+			go t.forward(localConn, t.sshConn, close)
+			t.connMap = append(t.connMap, close)
+		}
+	}()
+
+	return t.localPort, nil
+}
+
+// Close terminates the SSH tunnel
+func (t *Tunnel) Close() error {
+	// close the listener and the rest of the connections
+	err := t.listener.Close()
+	if err != nil {
+		return errors.Wrap(err, "Error while closing local tunnel listener")
+	}
+	for _, close := range t.connMap {
+		close <- true
+	}
+	err = t.sshConn.Close()
+	if err != nil {
+		return errors.Wrap(err, "Error while closing ssh tunnel connection")
+	}
+
+	return nil
+}
+
+func (t *Tunnel) forward(localConn net.Conn, sshConn *ssh.Client, close chan interface{}) {
+	t.log.Debug("New forwarded connection")
+	remoteConn, err := sshConn.Dial("tcp", t.target)
+	if err != nil {
+		t.log.Errorf("Failed to establish remote connection (%s) over SSH tunnel (%s): %s", t.target, t.sshHost, err)
+		return
+	}
+	copyConn := func(writer, reader net.Conn) {
+		_, err := io.Copy(writer, reader)
+		if err != nil {
+			t.log.Errorf("Failed to copy data over SSH tunnel (%s): %s", t.sshHost, err)
+		}
+	}
+	go copyConn(localConn, remoteConn)
+	go copyConn(remoteConn, localConn)
+	<-close
+	t.log.Debug("Close signal received, stopping forwarder")
+	_ = localConn.Close()
+	_ = remoteConn.Close()
+}
+
+// NewTunnel creates and returns an SSHTunnel
+func NewTunnel(sshHost string, sshUser string, sshAuth ssh.AuthMethod, tunnelTarget string, logger *logrus.Logger) *Tunnel {
+	return &Tunnel{sshHost: sshHost, sshUser: sshUser, sshAuth: sshAuth, target: tunnelTarget, log: logger}
+}
